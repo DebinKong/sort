a5b6790c577ea7d8242b5131643ed49af4d8b6cc
DebinKong <118550869+DebinKong@users.noreply.github.com>
2022-11-21 20:35:48 +0800

    Update Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -3,6 +3,7 @@
 
 class Utils {
 public:
+    static bool compare(std::vector<std::string>& v, int i, int j);
     static void InitVector(std::vector<int>& nums, int upper);
     static bool VerifySort(std::vector<int>& nums);
     static bool VerifySort(std::vector<int>& nums, int begin, int end);
